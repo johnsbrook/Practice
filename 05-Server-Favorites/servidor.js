926eb1e806171a0dbacffb9807388005b4c32601
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-21 21:12:35 -0400

    fixed issues with cases and paths

--- 05-Server-Favorites/servidor.js
+++ 05-Server-Favorites/servidor.js
@@ -1,47 +1,53 @@
 const http = require('http');
 const fs = require('fs');
 const PORT = 8080;
+
 const servidor = http.createServer(manejadorSolicitud);
+
     servidor.listen(PORT, () => {
         console.log('El servidor esta escuchando en el puerto ' + PORT + '. Se puede acceder a traves de la pagina http://localhost:' + PORT);
     })
 
 function manejadorSolicitud(solicitud, respuesta) {
+
     const camino = solicitud.url; 
 
     switch(camino) {
+
     case "/": 
-        fs.readFile(__dirname + '/index.html', 'utf8', function(err, data){
-            if(err) throw err;
-
-            respuesta.writeHead(200, {"Content-Type": "text/html"});
-            respuesta.end(data);
-        });
-        break;
-        
-    case "/food":
-        fs.readFile(__dirname + '/food.html', 'utf8', function(err, data){
-            if(err) throw err;
-
-            respuesta.writeHead(200, {'Content-Type': 'text/html'});
-            respuesta.end(data);
-        });
-        break;
-
-    case "/frameworks":
-        fs.readFile(__dirname, '/framework.html', 'utf8', function(err, data){
+        fs.readFile(__dirname + '/index.html', 'utf8', function(err, data) {
             if (err) throw err;
 
-            respuesta.writeHead(200, {'Content-Type': 'text/html'});
+            respuesta.writeHead(200, {"Content-Type": "text/html"})
             respuesta.end(data);
+        });
+        break;
+
+    case "/food":
+
+        fs.readFile(__dirname + '/food.html', 'utf8', function(err, data) {
+            if (err) throw err;
+
+            respuesta.writeHead(200, {"Content-Type": "text/html"})
+            respuesta.end(data);
+        });
+        break;
+
+
+    case "/frameworks":
+        fs.readFile(__dirname + '/frameworks.html', 'utf8', function(err, data) {
+            if (err) throw err;
+
+            respuesta.writeHead(200, {'Content-Type': 'text/html'})
+            respuesta.end(data)
         });
         break;
     
     case "/movies":
-        fs.readFile(__dirname, '/movies.html', 'utf8', function(err, data){
+        fs.readFile(__dirname + '/movies.html', 'utf8', function(err, data)  {
             if (err) throw err;
 
-            respuesta.writeHead(200, {'Content-Type': 'text/html'});
+            respuesta.writeHead(200, {'Content-Type': 'text/html'})
             respuesta.end(data);
         });
         break;
