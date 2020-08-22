683cdebb1f2b5810ad58c3631f38b47e1efda55e
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-21 20:43:19 -0400

    added index, food, frameworks and movies paths

--- 05-Server-Favorites/servidor.js
+++ 05-Server-Favorites/servidor.js
@@ -18,8 +18,30 @@ function manejadorSolicitud(solicitud, respuesta) {
             respuesta.end(data);
         });
     
-    case "/food"
+    case "/food":
+        fs.readFile(__dirname + '/food.html', function(err, data){
+            if(err) throw err;
+
+            respuesta.writeHead(200, {'Content-Type': 'text/html'});
+            respuesta.end(data);
+        })
+
+    case "/frameworks":
+        fs.readFile(__dirname, '/framework.html', function(err, data){
+            if (err) throw err;
+
+            respuesta.writeHead(200, {'Content-Type': 'text/html'});
+            respuesta.end(data);
+        })
+    
+    case "/movies":
+        fs.readFile(__dirname, '/movies.html', function(err, data){
+            if (err) throw err;
+
+            respuesta.writeHead(200, {'Content-Type': 'text/html'});
+            respuesta.end(data);
+        })
     }
+}
 
     
-}
