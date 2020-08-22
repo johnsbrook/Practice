b9b604107cb91343049424a6f35f77147a263db8
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-21 20:37:29 -0400

    added request handler)

--- 05-Server-Favorites/servidor.js
+++ 05-Server-Favorites/servidor.js
@@ -1,2 +1,25 @@
 const http = require('http');
 const fs = require('fs');
+const PORT = 8080;
+const servidor = http.createServer(manejadorSolicitud);
+    servidor.listen(PORT, () => {
+        console.log('El servidor esta escuchando en el puerto ' + PORT + '. Se puede acceder a traves de la pagina http://localhost:' + PORT);
+    })
+
+function manejadorSolicitud(solicitud, respuesta) {
+    const camino = solicitud.url; 
+
+    switch(camino) {
+    case "/": 
+        fs.readFile(__dirname + '/index.html', 'utf8', function(err, data){
+            if(err) throw err;
+
+            respuesta.writeHead(200, {"Content-Type": "text/html"});
+            respuesta.end(data);
+        });
+    
+    case "/food"
+    }
+
+    
+}
